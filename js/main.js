8c945523e314a5448e6d7b201feec5ce7b51abcf
Sarah Besozzi <sarah.besozzi@gmail.com>
2015-10-06 22:23:16 -0400

    Added overflow hidden to collapsePara

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 // Add event click to button
 
-$('button').on('click', function(event){
+$('button').on('click', function(event) {
   
   //
   var btn = $(event.target);
 
-// Collapse paragraph sibling of button
+// Collapse paragraph sibling of button 
+// Add class to paragraph
 
-  btn.siblings().addClass('collapsePar');
+  btn.siblings().addClass('collapsePara');
 
-  
+
+// Target siblings of button & remove and close 
+
  
-  btn.parent().siblings().find('normalPar').removeClass('normalPar').addClass('collapsePar');
-
-
+  btn.parent().siblings().find('normalPara').removeClass('normalPara').addClass('collapsePara');
 
 });
+
+
